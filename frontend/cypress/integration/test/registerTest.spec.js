6fee92eba174116eae46114f429ab8d987a8dd64
Marcos Khabie <marcos.khabie@ing.austral.edu.ar>
2019-05-23 17:44:48 -0300

    register test

---
+++ frontend/cypress/integration/test/registerTest.spec.js
@@ -0,0 +1,14 @@
+describe('Register test', function() {
+    it('should visit register page and register a course', function() {
+        cy.visit('http://localhost:3000');
+
+        cy.contains("Create course");
+
+        cy.get('[id="course-name"]').type('Matematica');
+        cy.get('[id="course-description"]').type('This is a description');
+        cy.get('[id="course-platform"]').type('Platform');
+        cy.get('[id="course-link"]').type('link.com');
+
+        cy.contains("SAVE").click()
+    })
+})
