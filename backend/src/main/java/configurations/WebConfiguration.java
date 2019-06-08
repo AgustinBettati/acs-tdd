fa23820870938fbc77d898d963ee27ff510e303c
Your Name <david.cerbino@ing.austral.edu.ar>
2019-06-07 22:45:03 -0300

    moved webconfiguration and chanched courseTest.spec.js still broken

--- backend/src/main/java/configurations/WebConfiguration.java
+++ backend/src/main/java/configurations/WebConfiguration.java
@@ -1,4 +1,4 @@
-package cursago;
+package configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
