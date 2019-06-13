4238fd9411096e1e53bbbe3acfb4c72615423684
Agustin Bettati <bettatiagustin@gmail.com>
2019-06-13 17:35:38 -0300

    adjusted test of listing

--- frontend/cypress/integration/test/courseTest.spec.js
+++ frontend/cypress/integration/test/courseTest.spec.js
@@ -26,35 +26,12 @@ describe('Register test', function() {
         cy.contains("Create course")
     });
 
-    it('should visit update page and bring all the respective info', function() {
-        cy.visit('/home');
+    it('should visit home page and show created course', function() {
+        cy.visit('http://localhost:3000/home');
 
-        cy.contains("ed");
-
-        cy.get('[id="course-name"]').should('be.empty');
-        cy.get('[id="course-description"]').should('be.empty');
-        cy.get('[id="course-platform"]').should('be.empty');
-        cy.get('[id="course-link"]').should('be.empty');
-        const id = cy.get('[id="course-id"]');
-        id.should('be.empty');
-        id.type('1');
-
-        cy.wait(500);
-        const courseName = cy.get('[id="course-name"]').should('not.have.value', '');
-        cy.get('[id="course-description"]').should('not.have.value', '');
-        cy.get('[id="course-platform"]').should('not.have.value' , '');
-        cy.get('[id="course-link"]').should('not.have.value' , '');
-
-        courseName.type('NUEVONOMBRE');
-        cy.contains("DELETE");
-        cy.contains("SAVE").click();
-
-        cy.url().should('include', '/home');
-
-        cy.contains('NUEVONOMBRE')
-
-
-
-
+        cy.contains("Matematica");
+        cy.contains("Platform");
     })
+
+    //TODO hacer test para el edit
 });
